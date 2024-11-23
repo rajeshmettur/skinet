8aa4f1af600f65d2152a09796d84367bb9811621
Muthunagai <muthus2008@gmail.com>
2024-11-23 14:18:57 +0530

    End of Section 10

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../../../shared/models/product';
 import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
@@ -12,7 +13,8 @@ import { MatIcon } from '@angular/material/icon';
     MatCardContent,
     MatCardActions,
     MatIcon,
-    CurrencyPipe
+    CurrencyPipe,
+    RouterLink
   ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
