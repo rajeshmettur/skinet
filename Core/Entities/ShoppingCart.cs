e43eaccf9d255f3e261cfb9eac814a5ebb78d113
Muthunagai <muthus2008@gmail.com>
2024-11-23 18:55:12 +0530

    End of Section 12

---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+namespace Core.Entities;
+
+public class ShoppingCart
+{
+    public required string Id { get; set; }  
+
+    public List<CartItem> Items { get; set; }  = [];
+}
