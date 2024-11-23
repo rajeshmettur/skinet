e5cf4655329b46ebcbc5e5669176c4463cf134d7
Muthunagai <muthus2008@gmail.com>
2024-11-23 07:50:21 +0530

    End of Section 8

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    data: Product[];
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
